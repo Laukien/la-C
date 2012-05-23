337b953b39671c14260bce0c427ae7223b372b0e
Stephan Laukien <software@laukien.com>
2012-05-23 09:35:19 +0200

    FIX: gcc-optimize-bug

--- la_console.c
+++ la_console.c
@@ -20,6 +20,7 @@
 #include <sys/ioctl.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
 
 void console_clean() {
 	printf ( CONSOLE_CLEAN );
@@ -51,4 +52,35 @@ int console_getHeight() {
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
 	return w.ws_ypixel;
+}
+
+int console_getKey(int byte, int second) {
+    struct termios orig_termios;                /* terminal structure */
+    volatile int ttyfd = STDIN_FILENO;          /* STDIN (0) */
+
+	/* save old state */
+    if (tcgetattr(ttyfd,&orig_termios) < 0) return -1;
+
+	/* switch to raw */
+    struct termios raw;
+    raw = orig_termios;                         /* copy original and then modify below */
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = byte; raw.c_cc[VTIME] = second;
+
+    if (tcsetattr(ttyfd,TCSAFLUSH,&raw) < 0) return -1;
+
+	/* read key form stdin */
+	int n;
+	int c;
+	n = read(ttyfd, &c, 1);
+	if (n < 0) c = -1;
+	else if (n == 0) c = 0;
+
+	/* reset state */
+    if (tcsetattr(ttyfd,TCSAFLUSH,&orig_termios) < 0) return -1;
+
+	return c;
 }
