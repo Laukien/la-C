e6779633ec7bc6a9431dd1482d42da9b7b2af2b1
Stephan Laukien <software@laukien.com>
2012-07-08 14:04:52 +0200

    FIX: memory leaks

--- example_stringbuffer_1.c
+++ example_stringbuffer_1.c
@@ -6,16 +6,19 @@ int main(void) {
 
 	stringbuffer_append(str, "Test");                 /* append string */
 	stringbuffer_append(str, "!");
-	printf ( "STRING: %s\n", stringbuffer_text(str) );
+	char *text;
+	text = stringbuffer_text(str);
+	printf ( "STRING: %s\n", text );
+	free(text);
 
 	stringbuffer_insert(str, "Das ist", 0);           /* insert string */
-	printf ( "STRING: %s\n", stringbuffer_text(str) );
+	printf ( "STRING: %s\n", str->text );
 
 	stringbuffer_insert(str, " ", 7);
-	printf ( "STRING: %s\n", stringbuffer_text(str) );
+	printf ( "STRING: %s\n", str->text );
 
 	stringbuffer_insert(str, "!!", stringbuffer_size(str));
-	printf ( "STRING: %s\n", stringbuffer_text(str) );
+	printf ( "STRING: %s\n", str->text );
 
 	printf ( "SIZE: %d\n", stringbuffer_size(str) );  /* show string-size */
 
