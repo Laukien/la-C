fc05647e5c960efdd1f386cd52b4a989411ffad8
Stephan Laukien <software@laukien.com>
2012-08-07 14:30:01 +0200

    ADD: exception

--- la_database_postgresql.c
+++ la_database_postgresql.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
@@ -19,16 +20,6 @@ char *database_postgresql_escapeParameter(const char *query) {
 	return result;
 }
 
-BOOL database_postgresql_checkSelf(DATABASE_POSTGRESQL *self) {
-	error_reset();
-	if (self == NULL) {
-		error_set(DATABASE_POSTGRESQL_ERROR_INIT, "database not initiated");
-		return FALSE;
-	}
-
-	return TRUE;
-}
-
 BOOL database_postgresql_checkParameter(DATABASE_POSTGRESQL *self) {
 	if (
 		self->host == NULL
@@ -41,7 +32,7 @@ BOOL database_postgresql_checkParameter(DATABASE_POSTGRESQL *self) {
 		||
 		self->name == NULL
 	) {
-		error_set(DATABASE_POSTGRESQL_ERROR_PARAMETER, "invalid parameter");
+		if (self->exception != NULL) exception_throw(self->exception, DATABASE_POSTGRESQL_ERROR_PARAMETER, "invalid parameter");
 		return FALSE;
 	}
 
@@ -49,12 +40,13 @@ BOOL database_postgresql_checkParameter(DATABASE_POSTGRESQL *self) {
 }
 
 DATABASE_POSTGRESQL *database_postgresql_new() {
-	error_init();
 	DATABASE_POSTGRESQL *self = (DATABASE_POSTGRESQL *)malloc(sizeof(DATABASE_POSTGRESQL));
 	if (self == NULL) {
-		error_set(DATABASE_POSTGRESQL_ERROR_MEMORY, "init memory");
+		if (self->exception != NULL) exception_throw(self->exception, DATABASE_POSTGRESQL_ERROR_MEMORY, "init memory");
+		return self;
 	}
 
+	self->exception = NULL;
 	self->host = NULL;
 	self->port = 5432;
 	self->name = NULL;
@@ -70,8 +62,15 @@ DATABASE_POSTGRESQL *database_postgresql_new() {
 	return self;
 }
 
+void database_setException(DATABASE_POSTGRESQL *self, EXCEPTION *e) {
+	assert(self != NULL);
+	assert(e != NULL);
+
+	self->exception = e;
+}
+
 void database_postgresql_free(DATABASE_POSTGRESQL *self) {
-	if (!database_postgresql_checkSelf(self)) return;
+	assert(self != NULL);
 
 	/* close objects */
 	if (database_postgresql_isResult(self)) database_postgresql_closeResult(self);
@@ -90,11 +89,11 @@ void database_postgresql_free(DATABASE_POSTGRESQL *self) {
 }
 
 void database_postgresql_open(DATABASE_POSTGRESQL *self) {
-	if (!database_postgresql_checkSelf(self)) return;
+	assert(self != NULL);
 	if (!database_postgresql_checkParameter(self)) return;
 
 	if (database_postgresql_isOpen(self)) {
-		error_set(DATABASE_POSTGRESQL_ERROR_CONNECTION, "connection already opened");
+		if (self->exception != NULL) exception_throw(self->exception, DATABASE_POSTGRESQL_ERROR_CONNECTION, "connection already opened");
 		return;
 	}
 
@@ -104,69 +103,75 @@ void database_postgresql_open(DATABASE_POSTGRESQL *self) {
 }
 
 void database_postgresql_close(DATABASE_POSTGRESQL *self) {
-	if (!database_postgresql_checkSelf(self)) return;
+	assert(self != NULL);
 
 	if (database_postgresql_isOpen(self)) {
 		if (database_postgresql_isResult(self)) database_postgresql_closeResult(self);
 		PQfinish(self->connection);
 		self->connection = NULL;
 	} else {
-		error_set(DATABASE_POSTGRESQL_ERROR_CONNECTION, "connection already closed");
+		if (self->exception != NULL) exception_throw(self->exception, DATABASE_POSTGRESQL_ERROR_CONNECTION, "connection already closed");
 	}
 }
 
 BOOL database_postgresql_isOpen(DATABASE_POSTGRESQL *self) {
-	if (!database_postgresql_checkSelf(self)) return FALSE;
+	assert(self != NULL);
 
 	return self->connection != NULL;
 }
 
 void database_postgresql_setHost(DATABASE_POSTGRESQL *self, const char *host) {
-	if (!database_postgresql_checkSelf(self)) return;
+	assert(self != NULL);
+	assert(host != NULL);
 
 	self->host = (char *)malloc(strlen(host) + 1);
 	strcpy(self->host, host);
 }
 
 void database_postgresql_setPort(DATABASE_POSTGRESQL *self, int port) {
-	if (!database_postgresql_checkSelf(self)) return;
+	assert(self != NULL);
+	assert(port > 0);
 
 	self->port = port;
 }
 
 void database_postgresql_setName(DATABASE_POSTGRESQL *self, const char *name) {
-	if (!database_postgresql_checkSelf(self)) return;
+	assert(self != NULL);
+	assert(name != NULL);
 
 	self->name = (char *)malloc(strlen(name) + 1);
 	strcpy(self->name, name);
 }
 
 void database_postgresql_setUser(DATABASE_POSTGRESQL *self, const char *user) {
-	if (!database_postgresql_checkSelf(self)) return;
+	assert(self != NULL);
+	assert(user != NULL);
 
 	self->user = (char *)malloc(strlen(user) + 1);
 	strcpy(self->user, user);
 }
 
 void database_postgresql_setPassword(DATABASE_POSTGRESQL *self, const char *password) {
-	if (!database_postgresql_checkSelf(self)) return;
+	assert(self != NULL);
+	assert(password != NULL);
 
 	self->password = (char *)malloc(strlen(password) + 1);
 	strcpy(self->password, password);
 }
 
 void database_postgresql_setSchema(DATABASE_POSTGRESQL *self, const char *schema) {
-	if (!database_postgresql_checkSelf(self)) return;
+	assert(self != NULL);
+	assert(schema != NULL);
 
 	self->schema = (char *)malloc(strlen(schema) + 1);
 	strcpy(self->schema, schema);
 }
 
 void database_postgresql_closeResult(DATABASE_POSTGRESQL *self) {
-	if (!database_postgresql_checkSelf(self)) return;
+	assert(self != NULL);
 
 	if (!database_postgresql_isResult(self)) {
-		error_set(DATABASE_POSTGRESQL_ERROR_RESULT, "result is closed");
+		if (self->exception != NULL) exception_throw(self->exception, DATABASE_POSTGRESQL_ERROR_RESULT, "result is closed");
 		return;
 	}
 
@@ -178,7 +183,7 @@ void database_postgresql_closeResult(DATABASE_POSTGRESQL *self) {
 }
 
 BOOL database_postgresql_isResult(DATABASE_POSTGRESQL *self) {
-	if (!database_postgresql_checkSelf(self)) return FALSE;
+	assert(self != NULL);
 
 	return self->result != NULL;
 }
@@ -220,19 +225,19 @@ BOOL database_postgresql_lastResult(DATABASE_POSTGRESQL *self) {
 }
 
 int database_postgresql_getResultRowCount(DATABASE_POSTGRESQL *self) {
-	if (!database_postgresql_checkSelf(self)) return 0;
+	assert(self != NULL);
 
 	return self->resultRow;
 }
 
 int database_postgresql_getResultColumnCount(DATABASE_POSTGRESQL *self) {
-	if (!database_postgresql_checkSelf(self)) return 0;
+	assert(self != NULL);
 
 	return self->resultCol;
 }
 
 int database_postgresql_getResultCursor(DATABASE_POSTGRESQL *self) {
-	if (!database_postgresql_checkSelf(self)) return 0;
+	assert(self != NULL);
 
 	return self->resultCur;
 }
@@ -240,15 +245,15 @@ int database_postgresql_getResultCursor(DATABASE_POSTGRESQL *self) {
 char *database_postgresql_getString(DATABASE_POSTGRESQL *self, int col) {
 	if (!database_postgresql_isResult(self)) return NULL;
 	if (self->resultCur < 0 || self->resultCur >= self->resultRow) {
-		error_set(DATABASE_POSTGRESQL_ERROR_PARAMETER, "row out of range");
+		if (self->exception != NULL) exception_throw(self->exception, DATABASE_POSTGRESQL_ERROR_PARAMETER, "row out of range");
 		return NULL;
 	}
 	if (col < 0 || col >= self->resultCol) {
-		error_set(DATABASE_POSTGRESQL_ERROR_PARAMETER, "column out of range");
+		if (self->exception != NULL) exception_throw(self->exception, DATABASE_POSTGRESQL_ERROR_PARAMETER, "column out of range");
 		return NULL;
 	}
 	if (PQbinaryTuples(self->result)) {
-		error_set(DATABASE_POSTGRESQL_ERROR_FORMAT, "data format");
+		if (self->exception != NULL) exception_throw(self->exception, DATABASE_POSTGRESQL_ERROR_FORMAT, "data format");
 		return NULL;
 	}
 	
@@ -256,7 +261,7 @@ char *database_postgresql_getString(DATABASE_POSTGRESQL *self, int col) {
 	char *dbres = PQgetvalue(self->result, self->resultCur, col);
 	char *res = (char *)malloc(strlen(dbres) + 1);
 	if(res == NULL) {
-		error_set(DATABASE_POSTGRESQL_ERROR_MEMORY, "out of memory");
+		if (self->exception != NULL) exception_throw(self->exception, DATABASE_POSTGRESQL_ERROR_MEMORY, "out of memory");
 		return NULL;
 	}
 	strcpy(res, dbres);
@@ -265,7 +270,7 @@ char *database_postgresql_getString(DATABASE_POSTGRESQL *self, int col) {
 }
 
 void database_postgresql_execute(DATABASE_POSTGRESQL *self, const char *query, ...) {
-	if (!database_postgresql_checkSelf(self)) return;
+	assert(self != NULL);
 		
 	/* reset */
 	if (database_postgresql_isResult(self)) database_postgresql_closeResult(self);
@@ -313,12 +318,12 @@ void database_postgresql_execute(DATABASE_POSTGRESQL *self, const char *query, .
 	stringbuffer_append(sb, text + idx);
 
 	va_end(args);
-
-	self->result = PQexec(self->connection, stringbuffer_text(sb));
-	stringbuffer_free(sb);
 	free(text);
+
+	self->result = PQexec(self->connection, sb->text);
+	stringbuffer_free(sb);
 	if (self->result == NULL) {
-		error_set(DATABASE_POSTGRESQL_ERROR_EXECUTE, "unable to execute");
+		if (self->exception != NULL) exception_throw(self->exception, DATABASE_POSTGRESQL_ERROR_EXECUTE, "unable to execute");
 		return;
 	}
 
@@ -330,7 +335,7 @@ void database_postgresql_execute(DATABASE_POSTGRESQL *self, const char *query, .
 			self->resultCol = PQnfields(self->result);
 			return;
 		default:
-			error_set(DATABASE_POSTGRESQL_ERROR_EXECUTE, PQresultErrorMessage(self->result));
+			if (self->exception != NULL) exception_throw(self->exception, DATABASE_POSTGRESQL_ERROR_EXECUTE, PQresultErrorMessage(self->result));
 	}	
 }
 
