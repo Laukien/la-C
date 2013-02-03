4fd8fc0e84cd4cdf375579e40a221ac83a298eb2
Stephan Laukien <software@laukien.com>
2013-02-03 19:37:54 +0100

    UPD: version

--- la_common.h
+++ la_common.h
@@ -5,7 +5,7 @@
  *
  *    Description:  common definitions
  *
- *        Version:  1.3.9
+ *        Version:  1.3.10
  *        Created:  03/09/2012 09:40:12 AM
  *       Revision:  none
  *       Compiler:  gcc
@@ -25,7 +25,7 @@ extern "C" {
 
 /* info */
 #define LA_NAME "la-C"
-#define LA_VERSION "1.3.9"
+#define LA_VERSION "1.3.10"
 #define LA_AUTHOR "Stephan Laukien"
 
 /* boolean type */
