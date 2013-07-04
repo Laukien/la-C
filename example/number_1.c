bf03f99cdedd1f5af2795d5ad01d24aa750fb38d
Stephan Laukien <software@laukien.com>
2013-07-04 16:21:45 +0200

    FIX: new format

--- example/number_1.c
+++ example/number_1.c
@@ -4,13 +4,13 @@
 
 int main(void) {
 	char *num_i;
-	num_i = number_intToString(-1234);
-	printf ( "Number: %s (%d)\n", num_i, (int)number_intToSize(-1234) );
+	num_i = number_integerToString(-1234);
+	printf ( "Number: %s (%d)\n", num_i, (int)number_getIntegerLength(-1234) );
 	free(num_i);
 
 	char *num_ui;
-	num_ui = number_uintToString(1234);
-	printf ( "Number: %s (%d)\n", num_ui, (unsigned int)number_uintToSize(1234) );
+	num_ui = number_unsignedIntegerToString(1234);
+	printf ( "Number: %s (%d)\n", num_ui, (unsigned int)number_getUnsignedIntegerLength(1234) );
 	free(num_ui);
 
 	return 0;
