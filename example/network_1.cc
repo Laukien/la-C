8231c3063c67e30777b195178dda2005bec62a9f
Stephan Laukien <stephan.laukien@siv.de>
2014-02-03 15:51:41 +0100

    UPD: made network_1 nice

--- example/network_1.cc
+++ example/network_1.cc
@@ -5,18 +5,29 @@ using namespace std;
 using namespace la::network;
 
 int main() {
+	string host = "en.wikipedia.org";           /* wikipedia */
 	string word;
 
-	cout << "Enter one word-search: " << flush;
+	cout << "Searching Wikipedia: " << flush;
 	cin >> word;
+	word[0] = toupper(word[0]);                 /* upper first letter */
 
 	Network net;
-	net.setAddress("www.google.com");
+	net.setAddress(host);
 	net.setPort(80);
 	net.setTimeout(5);
 	net.open();
-	net.writeString("GET /search?q=" + word + " HTTP 1.1");
+	net.writeString("GET /wiki/" + word + " HTTP/1.1");
+	net.writeString("Host: " + host);
+	net.writeString("User-Agent: ELinks/0.12pre5 (textmode; Linux; 167x55-2)");
+	net.writeString("Accept: */*");
+	net.writeString("Connection: Keep-Alive");
+	net.writeString("Accept-Language: en");
+	net.writeString("");
+
 	string page = net.readString();
-	cout << page << endl;
+
+	cout << page << endl;                       /* show result */
+
 	net.close();
 }
